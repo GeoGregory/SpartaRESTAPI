f403d2de5534e6d4096f581b24940821e2405322
George Gregory <georgegregory@live.co.uk>
2022-02-07 11:17:54 +0000

    Creating the Project / First push

---
+++ src/main/java/com/sparta/api/spartarestapi/SpartaRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.api.spartarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/api/spartarestapi/SpartaRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.api.spartarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
