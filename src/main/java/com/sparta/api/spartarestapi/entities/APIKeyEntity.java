7e9eed2777bb773e5bf93a22487ee63681a81189
Alexander Jarrett <alexjarrett400@gmail.com>
2022-02-10 14:38:29 +0000

    Added ADMIN API Key, randomly generated but does not decay yet

--- src/main/java/com/sparta/api/spartarestapi/entities/APIKeyEntity.java
+++ src/main/java/com/sparta/api/spartarestapi/entities/APIKeyEntity.java
@@ -11,6 +11,8 @@ public class APIKeyEntity {
     private String id;
     @Field("API_KEY")
     private String APIKey;
+    private String username;
+    private String password;
 
     public String getId() {
         return id;
@@ -26,6 +28,22 @@ public class APIKeyEntity {
 
     public void setAPIKey(String API_KEY) {
         this.APIKey = API_KEY;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public APIKeyEntity() {
